2b6ca26429564d7cb79e395a735b8002656258b0
Felipe Bona <felipechiodinibona@hotmail.com>
2024-09-14 14:28:53 -0300

    ajusts

--- src/views/Auth/Registration.vue
+++ src/views/Auth/Registration.vue
@@ -4,18 +4,19 @@
       <img width="150px" src="/logo.png">
       <h1>Cadastre-se</h1>
       <p>Crei sua conta grátis e começe seu delivery com a RapidEats.</p>
-      <form class="d-flex flex-column" @submit.prevent="onSubmit">
+      <form class="d-flex flex-column" novalidate @submit.prevent="onSubmit">
         <label for="nome">Nome</label>
         <input class="form-control" type="text" required id="nome" v-model="form.name" />
+        <small style="color: red;">{{ errors.get('name') }}</small>
         <label for="login-cellphone" class="mt-2">Celular</label>
         <input class="form-control" required id="login-cellphone" type="tel" v-model="form.cellphone" />
-        <BaseError message="Informe o nome" />
+        <small style="color: red;">{{ errors.get('cellphone') }}</small>
         <label for="login-email">Email</label>
         <input class="form-control" required id="login-email" type="email" v-model="form.email" />
-        <BaseError message="Informe o email" />
+        <small style="color: red;">{{ errors.get('email') }}</small>
         <label for="login-password" class="mt-2">Senha</label>
         <input class="form-control" required id="login-password" type="password" v-model="form.password" />
-        <BaseError message="Informe a senha" />
+        <small style="color: red;">{{ errors.get('password') }}</small>
         <div class="mt-2">
           <small>
             This site is protected by reCAPTCHA and the Google
@@ -48,6 +49,7 @@ import { mapActions } from 'pinia'
 import { useUserStore } from '@/stores/user.js'
 import { request } from '@/js/api.js'
 import BaseError from '@/components/BaseError.vue'
+import useValidationErrors from '@/js/useValidationErrors'
 
 export default {
   components: {
@@ -65,7 +67,7 @@ export default {
         password: null,
         cellphone: null
       },
-      error: null,
+      errors: new useValidationErrors(),
       submiting: false,
     }
   },
@@ -84,7 +86,7 @@ export default {
 
         this.$router.push({ name: 'stores.choose' })
       } catch (error) {
-        this.error = error.response.data.message
+        this.errors.record(error.response.data.errors)
       }
 
       this.submiting = false
