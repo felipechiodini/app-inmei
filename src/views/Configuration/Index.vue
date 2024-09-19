cc082250e7fcaa8ac76d648140f8ad7922a1a389
Felipe Bona <felipechiodinibona@hotmail.com>
2024-09-18 22:07:24 -0300

    ajusts

--- src/views/Configuration/Index.vue
+++ src/views/Configuration/Index.vue
@@ -1,9 +1,18 @@
 <template>
   <BaseIndex title="Configurações">
     <form @submit.prevent="onSubmit()">
-      <label for="">Pedido Minimo</label>
-      <input class="form-control" type="text">
-
+      <div>
+        <label for="">Pedido Minimo</label>
+        <input class="form-control" v-model="form.minimum_order_value" type="text">
+      </div>
+      <div>
+        <label for="">Tempo para Entrega</label>
+        <input class="form-control" v-model="form.delivery" type="text">
+      </div>
+      <div>
+        <label for="">Tempo para Retidada</label>
+        <input class="form-control" v-model="form.withdrawal" type="text">
+      </div>
       <button type="submit" class="btn btn-primary mt-3">
         Salvar
       </button>
@@ -17,6 +26,15 @@ import BaseIndex from '@/components/BaseIndex.vue'
 export default {
   components: {
     BaseIndex
+  },
+  data() {
+    return {
+      form: {
+        minimum_order_value: null,
+        delivery: null,
+        withdrawal: null
+      }
+    }
   },
   methods: {
     onSubmit() {
