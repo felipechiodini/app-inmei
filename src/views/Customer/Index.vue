b6040392553370ffff87813e647365da9988bad2
Felipe Bona <felipechiodinibona@hotmail.com>
2024-09-25 21:26:00 -0300

    ajusts

--- src/views/Customer/Index.vue
+++ src/views/Customer/Index.vue
@@ -5,9 +5,15 @@
         Novo Cliente
       </button>
     </template>
-    <BaseTable request="customer">
-      <template #content="{ rows }">
-        <tr v-for="(customer, key) in rows" :key="key">
+    <div class="table-responsive">
+      <table class="table">
+        <tr>
+          <td>Nome</td>
+          <td>CPF</td>
+          <td>Celular</td>
+          <td>Criado em</td>
+        </tr>
+        <tr v-for="(customer, key) in customers" :key="key">
           <td>
             {{ customer.name }}
           </td>
@@ -18,24 +24,40 @@
             {{ customer.cellphone }}
           </td>
           <td>
-            <router-link :to="{ name: 'customer.update', params: { customer_id: customer.id  }}">
-              Editar
-            </router-link>
+            {{ customer.created_at }}
           </td>
         </tr>
-      </template>
-    </BaseTable>
+      </table>
+    </div>
   </BaseIndex>
 </template>
 
 <script>
 import BaseIndex from '@/components/BaseIndex.vue'
 import BaseTable from '@/components/BaseTable.vue'
+import { requesFromStore } from '@/js/api';
 
 export default {
   components: {
     BaseTable,
     BaseIndex
+  },
+  mounted() {
+    this.load()
+  },
+  data() {
+    return {
+      customers: []
+    }
+  },
+  methods: {
+    load() {
+      requesFromStore()
+        .get('customer')
+        .then(({ data }) => {
+          this.customers = data.customers
+        })
+    }
   }
 }
 </script>
